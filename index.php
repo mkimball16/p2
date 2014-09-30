8e28f3496ba4adad06a41320aabbb41b80f5c9c0
MacKenzie Kimball <mackenziekimball@gmail.com>
2014-09-30 19:51:01 -0400

    Added code to keep info after submit

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 <body>
 		<h1>Password Slinger</h1>
 		<h2>xkcd Password Generator</h2>
-
 	<p class="intro">Howdy! Welcome to my random password generator. Let's face it, coming up 
 	with a secure password is getting more and more difficult these days. But with so much
 	of our personal information online, and some very intelligent hackers, ensuring your information
@@ -100,24 +99,24 @@
 			<form method="POST" action="index.php">
 			<p>Number of words:<br />
 			<select name="passwordlength">
-					<option value="1">1</option>
-					<option value="2">2</option>
-			    	<option value="3">3</option>
-					<option value="4">4</option>
-			    	<option value="5">5</option>
+					<option value="1" <?php echo ($_POST['passwordlength'] == '1') ? ' selected="selected"' : '';?>>1</option>
+					<option value="2" <?php echo ($_POST['passwordlength'] == '2') ? ' selected="selected"' : '';?>>2</option>
+			    	<option value="3" <?php echo ($_POST['passwordlength'] == '3') ? ' selected="selected"' : '';?>>3</option>
+					<option value="4" <?php echo ($_POST['passwordlength'] == '4') ? ' selected="selected"' : '';?>>4</option>
+			    	<option value="5" <?php echo ($_POST['passwordlength'] == '5') ? ' selected="selected"' : '';?>>5</option>
 			</select></p>
 			<p>Would you like to make the first letter uppercase?<br />
-			<input type="radio" name="case" value="1">Yes<br />
-			<input type="radio" name="case" value="0" checked>No<br /></p>
+			<input type="radio" name="case" value="1" <?php if (isset($_POST['case']) && $_POST['case'] == '1')echo 'checked="checked"';?>>Yes<br />
+			<input type="radio" name="case" value="0" <?php if (isset($_POST['case']) && $_POST['case'] == '0')echo 'checked="checked"';?>>No<br /></p>
 			<p>Would you like the first word in all uppercase?<br />
-			<input type="radio" name="caps" value="1">Yes<br />
-			<input type="radio" name="caps" value="0" checked>No<br /></p>
+			<input type="radio" name="caps" value="1"<?php if (isset($_POST['caps']) && $_POST['caps'] == '1')echo 'checked="checked"';?>>Yes<br />
+			<input type="radio" name="caps" value="0" <?php if (isset($_POST['caps']) && $_POST['caps'] == '0')echo 'checked="checked"';?>>No<br /></p>
 			<p>Would you like to include a symbol?<br />
-			<input type="radio" name="symbols" value="1">Yes<br />
-			<input type="radio" name="symbols" value="0" checked>No<br /></p>
+			<input type="radio" name="symbols" value="1" <?php if (isset($_POST['symbols']) && $_POST['symbols'] == '1')echo 'checked="checked"';?>>Yes<br />
+			<input type="radio" name="symbols" value="0" <?php if (isset($_POST['symbols']) && $_POST['symbols'] == '0')echo 'checked="checked"';?>>No<br /></p>
 			<p>Would you like to include a number?<br />
-			<input type="radio" name="numbers" value="1">Yes<br />
-			<input type="radio" name="numbers" value="0" checked>No<br /></p>
+			<input type="radio" name="numbers" value="1" <?php if (isset($_POST['numbers']) && $_POST['numbers'] == '1')echo 'checked="checked"';?>>Yes<br />
+			<input type="radio" name="numbers" value="0" <?php if (isset($_POST['numbers']) && $_POST['numbers'] == '0')echo 'checked="checked"';?>>No<br /></p>
 			<input type="submit" class="submit" name="submit">
 		</form>
 			
@@ -125,6 +124,7 @@
 			}
 			echo "<br><br>";
 			?>
+
 			
 	<img id="password-gen" src="http://imgs.xkcd.com/comics/password_strength.png" alt="password">		
 </body>
